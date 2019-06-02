bbbb8ae4b95c2e7024b251f9475e9f2f54c191e6
Cooper Viktor <im.cooperviktor@gmail.com>
2019-06-02 10:25:01 +1000

    refactoring and keycode inputs

--- tetris.js
+++ tetris.js
@@ -1,21 +1,79 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-//* setting the size and colour of backdrop
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+context.scale(20, 20);
 
 //* tetris block 
 const matrix = [
   [0, 0, 0],
   [1, 1, 1],
-  [0, 1, 1]
+  [0, 1, 0]
 ];
-matrix.forEach((row, y) => {
-  row.forEach((value, x) =>{
-    if(value !== 0) {
-      context.fillStyle = 'red';
-      context.fillRect(x, y, 1, 1);
-    }
+
+function createMatrix(w, h) {
+  const matric = [];
+  while(h--) {
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
+function draw() {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawMatrix(player.matrix, player.pos);
+};
+
+const arena = createMatrix(12, 20);
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) =>{
+      if(value !== 0) {
+        context.fillStyle = 'red';
+        context.fillRect(x + offset.x,
+                         y + offset.y, 
+                         1, 1);
+      }
+    });
   });
-});
+};
+
+function playerDrop() {
+  player.pos.y++;
+  dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+  dropCounter += deltaTime;
+
+  if(dropCounter > dropInterval) {
+    playerDrop()
+  };
+
+  draw();
+  requestAnimationFrame(update)
+};
+
+const player = {
+  pos: {x: 5, y: 5},
+  matrix: matrix
+}
+
+document.addEventListener('keydown', event => {
+  if(event.keyCode === 37) {
+    player.pos.x--;
+  } else if(event.keyCode === 39) {
+    player.pos.x++;
+  } else if(event.keyCode === 40) {
+    playerDrop();
+  }
+})
+update()
